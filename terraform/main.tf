1a2d2b89b7d8d227183f16e70e1b1364056c572e
Matias Lagos <lagos.barra.m@gmail.com>
2025-08-28 18:40:44 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,18 @@
+########################################
+# Provider
+########################################
 provider "aws" {
   region = "us-east-1"
 }
 
-# Security Group que permite SSH solo desde tu IP
+########################################
+# Security Group: solo SSH desde tu IP
+########################################
 resource "aws_security_group" "ssh_sg" {
   name        = "ssh-only"
-  description = "Allow SSH only from my IP"
+  description = "Allow SSH only from my IP (22/tcp)"
 
+  # SSH solo desde tu IP /32
   ingress {
     description = "SSH from my IP"
     from_port   = 22
@@ -15,22 +21,30 @@ resource "aws_security_group" "ssh_sg" {
     cidr_blocks = [var.my_ip_cidr]
   }
 
+  # Salida libre
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "ssh-only"
+  }
 }
 
-# Instancia EC2
+########################################
+# EC2 (Ubuntu 22.04 LTS, us-east-1)
+########################################
 resource "aws_instance" "vm" {
-  ami           = "ami-08c40ec9ead489470" # Ubuntu 22.04 LTS en us-east-1
-  instance_type = "t2.micro"
-  key_name      = var.key_pair_name
-  security_groups = [aws_security_group.ssh_sg.name]
+  ami                    = "ami-08c40ec9ead489470" # Ubuntu 22.04 LTS (us-east-1)
+  instance_type          = "t2.micro"
+  key_name               = var.key_pair_name
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
 
   tags = {
     Name = "CloudSecurityDemo"
   }
 }
+
