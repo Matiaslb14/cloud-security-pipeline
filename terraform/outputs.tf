8a1edef3786dfffa4a4bb19654915c56d95b924d
Matias Lagos <lagos.barra.m@gmail.com>
2025-08-28 18:41:56 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,21 @@
+########################################
+# Outputs
+########################################
+
+# IP pública de la instancia
 output "instance_public_ip" {
   description = "IP pública de la instancia EC2"
-  value       = aws_instance.web.public_ip
+  value       = aws_instance.vm.public_ip
 }
 
-output "s3_bucket_name" {
-  description = "Nombre del bucket S3 creado"
-  value       = aws_s3_bucket.my_bucket.id
+# DNS público de la instancia
+output "instance_public_dns" {
+  description = "DNS público de la instancia EC2"
+  value       = aws_instance.vm.public_dns
+}
+
+# Comando SSH listo para usar
+output "ssh_command" {
+  description = "Comando de conexión SSH"
+  value       = "ssh -i ~/.ssh/mati-key.pem ubuntu@${aws_instance.vm.public_dns}"
 }
