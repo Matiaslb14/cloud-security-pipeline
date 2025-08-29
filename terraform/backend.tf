10eaf817f86cacae7323b8af9309d188a88c0928
Matias Lagos <lagos.barra.m@gmail.com>
2025-08-28 23:22:27 -0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,10 @@
+# terraform/backend.tf
 terraform {
   backend "s3" {
-    bucket         = "matiaslb14-tfstate-use1"   # <-- usa el nombre real que creaste
-    key            = "cloud-security-pipeline/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    # dynamodb_table = "terraform-locks"         # si creaste la tabla
+    bucket = "matiaslb14-tfstate-ue1"                  # 👈 exacto como en S3
+    key    = "cloud-security-pipeline/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    # dynamodb_table = "matiaslb14-terraform-locks"    # opcional si creas la tabla
   }
 }
